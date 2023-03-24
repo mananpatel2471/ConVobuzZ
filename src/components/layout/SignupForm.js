1e9a19933e13c2dbce44e215dc89906e16a8fdda
Rishita Patel <100127809+RishitaPatel1@users.noreply.github.com>
2023-03-24 15:25:41 +0530

    Add files via upload

---
+++ src/components/layout/SignupForm.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "../Css.css";
+const SignupForm = () => {
+    return(
+    <>
+        <div className="Signup"> 
+        <div><img className="Logo"
+                   src="./images/icons/ConVobuzZ-logo.svg"/>
+        </div>
+
+        <div className="Text1">Create an Account</div>
+
+        <div>
+            <input className="Signup-1"
+                   type="text"
+                   placeholder="      Enter User Name"
+            />
+            <img className="Name"
+                  
+                  src="./images/icons/Name.svg"
+            />
+        </div>
+        <div>
+            <input className="Signup-2"
+                   type="text"
+                   placeholder="      Enter Email"
+            />
+            <img className="Email"
+                  
+                  src="./images/icons/Email.svg"
+            />
+        </div>
+        <div>
+            <input className="Signup-3"
+                   type="text"
+                   placeholder="      Enter Password"
+            />
+            <img className="Password"
+                  
+                  src="./images/icons/Password.svg"
+            />
+        </div>
+        <div>
+            <input className="Signup-4"
+                   type="text"
+                   placeholder="      Confirm Password"
+            />
+            <img className="Cpassword"
+                  
+                  src="./images/icons/password-check.svg"
+            />
+        </div>
+        <div>
+            <input className="Signup-5"
+                   type="mobile"
+                   placeholder="       Mobile no"
+            />
+            <img className="Mobioleno"
+                  
+                  src="./images/icons/MobileNO.svg"
+            />
+        </div>
+        <div>
+            <button className="Register" >Register</button>
+        </div>
+        <div className="Text2">Already Registered?</div>
+        <div className ="Text3">Sign in</div>
+        </div>
+    </>
+ );
+}
+export default SignupForm;
